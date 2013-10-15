757a6b6cdbaafc42274ad66e1d35bae6e3f2abf9
geeknbar <dorian.coffinet@gmail.com>
2013-10-15 11:08:43 +0200

    add test for files writer and reader

--- workspace/workspace_java/Syst_expert/src/Test.java
+++ workspace/workspace_java/Syst_expert/src/Test.java
@@ -3,15 +3,12 @@ import java.io.IOException;
 import gestionFichier.FileWR;
 
 
-
 public class Test {
-	
-	public static void main(String[] args) throws IOException {
-		FileWR f= new FileWR();
-		String s = "Bonjour";
-		f.writeLine(s, "test.txt");
-		f.realLines("test.txt");
-		
-	}
 
+	public static void main (String[] args) throws IOException{
+		FileWR f = new FileWR();
+		
+		f.writeLine("Bonjour", "text.txt");
+		f.realLines("text.txt");
+	}
 }
--- workspace/workspace_java/Syst_expert/src/gestionFichier/FileWR.java
+++ workspace/workspace_java/Syst_expert/src/gestionFichier/FileWR.java
@@ -10,7 +10,7 @@ public class FileWR {
 
 	private Path pathSource;
 	private Path pathCible;
-	private List<String> lignes = new ArrayList<>();
+	private List<String> lignes = new ArrayList<String>();
 
 	public FileWR(){
 
