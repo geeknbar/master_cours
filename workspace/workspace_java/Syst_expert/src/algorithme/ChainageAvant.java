9a7238a3ec4fdaa182a39f04eeba9dd7daad321a
geeknbar <dorian.coffinet@gmail.com>
2013-10-15 11:44:19 +0200

    add methods CAv

--- workspace/workspace_java/Syst_expert/src/algorithme/ChainageAvant.java
+++ workspace/workspace_java/Syst_expert/src/algorithme/ChainageAvant.java
@@ -3,6 +3,9 @@
  */
 package algorithme;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Mickael
  *
@@ -15,8 +18,33 @@ public class ChainageAvant
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	
-	
+	public void verifiationChainageAvant(){
+		List BR = null;
+		List Demandable;
+		List BF;
+		ArrayList<String> VF;
+		ArrayList<String> VA = null;
+		ArrayList<String> VC;
+		boolean inf = true;
+		boolean dec = true;
+		int nbinf = 0;
+		
+		while (inf){
+			inf = false;
+			for (int r = 0; r < BR.size(); r++) {
+				dec = true;
+				for (int m = 0; m < VA.size(); m++) {
+					while (dec){
+//						if(r && (VF.contains(m) != VA.contains(m)) || m){
+//							dec = false;
+//						}
+					}
+				}
+				if (dec){
+					
+				}
+			}
+		}
+	}
 	
 }
