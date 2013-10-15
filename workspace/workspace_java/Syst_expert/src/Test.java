fdfaaeb39dcae5074a68f423f53b7cc3fe0f584a
geeknbar <dorian.coffinet@gmail.com>
2013-10-15 10:51:38 +0200

    test write aand read methods

--- workspace/workspace_java/Syst_expert/src/Test.java
+++ workspace/workspace_java/Syst_expert/src/Test.java
@@ -1,4 +1,17 @@
+import java.io.IOException;
+
+import gestionFichier.FileWR;
+
+
 
 public class Test {
+	
+	public static void main(String[] args) throws IOException {
+		FileWR f= new FileWR();
+		String s = "Bonjour";
+		f.writeLine(s, "test.txt");
+		f.realLines("test.txt");
+		
+	}
 
 }
--- workspace/workspace_java/Syst_expert/src/gestionFichier/FileWR.java
+++ workspace/workspace_java/Syst_expert/src/gestionFichier/FileWR.java
@@ -4,20 +4,21 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 public class FileWR {
 
 	private Path pathSource;
 	private Path pathCible;
-	private List<String> lignes;
+	private List<String> lignes = new ArrayList<>();
 
 	public FileWR(){
 
 	}
 
-	public void realLines() throws IOException{
-		List<String> lignes =  Files.readAllLines(  
-				FileSystems.getDefault().getPath("monfichier.txt"), StandardCharsets.UTF_8);  
+	public void realLines(String pathS) throws IOException{
+		setPathSource(pathS);
+		List<String> lignes =  Files.readAllLines(pathSource, StandardCharsets.UTF_8);  
 		for (String ligne : lignes)
 			System.out.println(ligne);
 	}
