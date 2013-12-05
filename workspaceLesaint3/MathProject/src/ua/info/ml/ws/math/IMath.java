4366a7b8dd5a52122364a7462729eb6ba1f56754
geeknbar <dorian.coffinet@gmail.com>
2013-12-05 19:32:04 +0100

    ajout WSDL/SOAP LESAINT

---
+++ workspaceLesaint3/MathProject/src/ua/info/ml/ws/math/IMath.java
@@ -0,0 +1,5 @@
+package ua.info.ml.ws.math;
+
+public interface IMath { 
+	public int add(int a, int b); 
+}
---
+++ workspaceLesaint3/MathProject/src/ua/info/ml/ws/math/Math.java
@@ -0,0 +1,2 @@
+package ua.info.ml.ws.math;
+public class Math implements IMath { public int add(int a, int b) { return a+b; }}
