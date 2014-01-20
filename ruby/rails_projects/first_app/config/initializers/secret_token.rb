4af03c80217af2420e41b9d698a70de3dfcb3e63
geeknbar <dorian.coffinet@gmail.com>
2014-01-20 15:35:28 +0100

    Commencement du tuto ruby pour le cours de Giluno.

---
+++ ruby/rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '064f1fe1a6b0317cec3b84c470f274c9248723cc5de1970673f5e2aeb7f8dacdb0f2adaf2731bf5dca9878794fd6d59b1cea0eedd715c013540ca69beaf8fc06'
