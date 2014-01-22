7f743a3ca30c3b453673cf429ff1c27d6bbf7dca
geeknbar <dorian.coffinet@gmail.com>
2014-01-22 13:50:54 +0100

    Fin section 4.

--- ruby/rails_projects/first_app/exemple_user.rb
+++ ruby/rails_projects/first_app/exemple_user.rb
@@ -9,4 +9,27 @@ class User
   def formatted_email
     "#{@nom} <#{@email}>"
   end
+
+  #exercices 4.5
+  def string_shuffle(s)
+	  s.split('').shuffle.join
+	end
+
+	class String
+	  def shuffle
+	     self.split('').shuffle.join
+	  end
+	end
+
+	def exercice3
+		personne1 = { :nom => "coffinet", :prenom => "dorian" }
+		personne2 = { :nom => "azerty", :prenom => "uiop" }
+		personne3 = { :nom => "qsdfgh", :prenom => "hjklm" }
+		params[:pere] = { personne1 }
+		params[:mere] = { personne2 }
+		params[:enfant] = { personne3 }
+
+		puts "#{params.inspect}"
+	end
+
 end
