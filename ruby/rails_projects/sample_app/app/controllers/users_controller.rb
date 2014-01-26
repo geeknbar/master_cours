d07d0031d2c467668852981a745bec26daefcdc0
geeknbar <dorian.coffinet@gmail.com>
2014-01-26 14:42:23 +0100

    ajout d'une page d'inscription. + test.

--- ruby/rails_projects/sample_app/app/controllers/users_controller.rb
+++ ruby/rails_projects/sample_app/app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
-	def new
+def new
+    @user = User.new
     @titre = "Inscription"
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+	def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Bienvenue dans l'Application Exemple!"
+      redirect_to @user
+    else
+      @titre = "Inscription"
+      render 'new'
+    end
   end
 end
